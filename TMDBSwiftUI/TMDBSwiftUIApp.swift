36e7d566df831d6110eea3bcfb7449a9fa206b71
Damian Cichosz <damcichosz@gmail.com>
2025-04-18 20:22:56 +0200

    Initial Commit

---
+++ TMDBSwiftUI/TMDBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBSwiftUIApp.swift
+//  TMDBSwiftUI
+//
+//  Created by Damian Cichosz on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TMDBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
